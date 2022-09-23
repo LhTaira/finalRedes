3e705c5762993aef753473e462147801595417e4
Eduardo Lima <eduardolima.df@gmail.com>
2022-09-22 23:08:53 -0300

    Utilizando Select para o controle de mensagens e conexoes

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import socket
 import argparse
 import os
 import json
+import select
 
 def findClient(target):
 
@@ -41,33 +42,35 @@ class Server(threading.Thread):
 
         sock.listen(1)
         print('Listening at', sock.getsockname())
-
+        inputs = [sock]
+        outputs = []
         while True:
 
+            readable, writable, exceptional = select.select(inputs, outputs, inputs)
+            for s in readable:
+                sc, sockname = sock.accept()
+                print('Accepted a new connection from {} to {}'.format(sc.getpeername(), sc.getsockname()))
+                
+                
+                server_socket = ServerSocket(sc, sockname, self)
+                
 
-            sc, sockname = sock.accept()
-            print('Accepted a new connection from {} to {}'.format(sc.getpeername(), sc.getsockname()))
-            
-            
-            server_socket = ServerSocket(sc, sockname, self)
-            
+                server_socket.start()
+                print(sockname, server_socket.sockname)
+                client = Client()
+                client.socket_name = sockname
+                client.room = 'sala1'
 
-            server_socket.start()
-            print(sockname, server_socket.sockname)
-            client = Client()
-            client.socket_name = sockname
-            client.room = 'sala1'
-
-            clients.append(client)
-            self.connections.append(server_socket)
-            print(clients)
-            print('Ready to receive messages from', sc.getpeername())
-            roomsObj = {
-                "header": "room",
-                "message": rooms
-            }
-            roomsJson = json.dumps(roomsObj)
-            server_socket.send(roomsJson)
+                clients.append(client)
+                self.connections.append(server_socket)
+                print(clients)
+                print('Ready to receive messages from', sc.getpeername())
+                roomsObj = {
+                    "header": "room",
+                    "message": rooms
+                }
+                roomsJson = json.dumps(roomsObj)
+                server_socket.send(roomsJson)
 
     def broadcast(self, message, source, name, isroom):
         client = findClient(source)
@@ -106,49 +109,53 @@ class ServerSocket(threading.Thread):
 
 
     def run(self):
-
+        inputs = [self.sc]
+        outputs = []
         while True:
-            message = self.sc.recv(1024).decode('ascii')
-            print('message: ', message)
-            if message:
-                print(message)
-                jsonMessage = json.loads(message)
-                if jsonMessage["header"] == "connect":
-                    newClient = Client()
-                    newClient.socket_name = self.sockname
-                    newClient.room = jsonMessage["message"]
+            readable, writable, exceptional = select.select(inputs, outputs, inputs)
+            for s in readable:
+                if s is not server:
+                    message = self.sc.recv(1024).decode('ascii')
+                    print('message: ', message)
+                    if message:
+                        print(message)
+                        jsonMessage = json.loads(message)
+                        if jsonMessage["header"] == "connect":
+                            newClient = Client()
+                            newClient.socket_name = self.sockname
+                            newClient.room = jsonMessage["message"]
 
-                    clientRoom = findClient(self.sockname)
-                    clients[clients.index(clientRoom)] = newClient
-                    
+                            clientRoom = findClient(self.sockname)
+                            clients[clients.index(clientRoom)] = newClient
+                            
 
-                elif jsonMessage["header"] == "disconnect":
-                    message = '{} has left the chat.'.format(jsonMessage["name"])
-                    self.server.broadcast(message, self.sockname, 'Server', 'false')
-                    roomsObj = {
-                        "header": "room",
-                        "message": rooms
-                        }
-                    roomsJson = json.dumps(roomsObj)
-                    self.server.broadcast(roomsJson, self.sockname,'','true')
+                        elif jsonMessage["header"] == "disconnect":
+                            message = '{} has left the chat.'.format(jsonMessage["name"])
+                            self.server.broadcast(message, self.sockname, 'Server', 'false')
+                            roomsObj = {
+                                "header": "room",
+                                "message": rooms
+                                }
+                            roomsJson = json.dumps(roomsObj)
+                            self.server.broadcast(roomsJson, self.sockname,'','true')
 
-                elif jsonMessage["header"] == "room":
-                    rooms.append(jsonMessage["message"])
-                    roomsObj = {
-                        "header": "room",
-                        "message": rooms
-                        }
-                    roomsJson = json.dumps(roomsObj)
-                    self.server.broadcast(roomsJson, self.sockname,'','true')
+                        elif jsonMessage["header"] == "room":
+                            rooms.append(jsonMessage["message"])
+                            roomsObj = {
+                                "header": "room",
+                                "message": rooms
+                                }
+                            roomsJson = json.dumps(roomsObj)
+                            self.server.broadcast(roomsJson, self.sockname,'','true')
 
-                else:
-                    self.server.broadcast(jsonMessage["message"], self.sockname,jsonMessage["name"],'false')
-            else:
+                        else:
+                            self.server.broadcast(jsonMessage["message"], self.sockname,jsonMessage["name"],'false')
+                    else:
 
-                print('{} has closed the connection'.format(self.sockname))
-                self.sc.close()
-                server.remove_connection(self)
-                return
+                        print('{} has closed the connection'.format(self.sockname))
+                        self.sc.close()
+                        server.remove_connection(self)
+                        return
 
     
     def send(self, message):
